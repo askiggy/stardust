7f0d5ac08031b9ec718951cfca4dec7a8cdfd48e
Benedicto de Jesus <dejesus.ben@gmail.com>
2021-08-25 15:35:50 -0700

    Branding component

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Branding from './components/Branding';
+
+export default { Branding };
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
+  entry: './src/index.js',
   output: {
-    filename: 'iggy-stardust.js'
+    filename: 'index.js'
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }]
+    rules: [
+      { test: /\.(txt)$/, use: 'raw-loader' },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+      // { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      // {
+      //   test: /\.css$/i,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     'style-loader',
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         modules: true
+      //       }
+      //     }
+      //   ]
+      // }
+    ]
   }
 };
